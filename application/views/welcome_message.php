1449bd79c5b4361020a9283337ddb270252839c7
MMKTechnologies <49527120+MMKTechnologies@users.noreply.github.com>
2020-10-29 00:03:00 +0500

    completed till 28 Oct

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -14,15 +14,28 @@
 		</script> -->
 
 
-		<!----> <script>
+		<script>
 			(function(p,u,s,h,x){p.webpushr=p.webpushr||function(){(p.webpushr.q=p.webpushr.q||[]).push(arguments)};h=u.getElementsByTagName('head')[0];x=u.createElement('script');x.async=1;x.src=s;h.appendChild(x);})(window,document,'https://www.browserpushnotifications.com/web-push.js');
 			webpushr('init', 'BG-ml47PihNJqz6zngrrflivKJt9HT0ftd1Djdx1EJrHjdnyUOuSgwJ_XoBptfVAKOJrEGG7EtzcoI5Dofrtcx4');
 		</script>
 			
-		<!-- start webpushr tracking code
-		<script>(function(w,d, s, id) {if(typeof(w.webpushr)!=='undefined') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.async=1;js.src = "https://cdn.webpushr.com/app.min.js";
-		fjs.parentNode.appendChild(js);}(window,document, 'script', 'webpushr-jssdk'));
-		webpushr('setup',{'key':'BE6iJir2Yny0yD499eJOh3YaDcuyrZla_YqQtI34f3zVoop5FDiGOsIMEDPE9GAK3kdcfPQLUUn3LLE5bIWAqhY' });</script>
-		end webpushr tracking code -->
+		<!-- start webpushr tracking code -->
+		<!-- <script>
+			(function(w,d, s, id) {if(typeof(w.webpushr)!=='undefined') return;w.webpushr=w.webpushr||function(){(w.webpushr.q=w.webpushr.q||[]).push(arguments)};var js, fjs = d.getElementsByTagName(s)[0];js = d.createElement(s); js.id = id;js.async=1;js.src = "https://cdn.webpushr.com/app.min.js";
+			fjs.parentNode.appendChild(js);}(window,document, 'script', 'webpushr-jssdk'));
+			webpushr('setup',{'key':'BE6iJir2Yny0yD499eJOh3YaDcuyrZla_YqQtI34f3zVoop5FDiGOsIMEDPE9GAK3kdcfPQLUUn3LLE5bIWAqhY' });</script> -->
+		<!-- end webpushr tracking code -->
+
+		<!-- <script>
+
+			function _webpushrScriptReady(){
+				webpushr('attributes',{"Gender" : "Male", "User ID" : "8742",  "E-mail" : "name@example2.com"});
+				webpushr('fetch_id',function (sid) { 
+					//save id to database
+					console.log('webpushr subscriber id: ' + sid)
+				});
+			}
+		</script> -->
+
 	</body>
 </html>
